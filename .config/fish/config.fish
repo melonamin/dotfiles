fd9300b53666fd0965525d00e0fbed435f108bc4
Alex Mazanov <alexandr.mazanov@gmail.com>
2022-09-03 19:39:29 -0400

    Add config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Load the shell dotfiles, and then some:
+# * ~/.path can be used to extend `$PATH`.
+# * ~/.extra can be used for other settings you don’t want to commit.
+for file in ~/.{path,bash_prompt,exports,aliases,functions,extra}
+    [ -r "$file" ] && [ -f "$file" ] && source "$file"
+end
+
+starship init fish | source
+
