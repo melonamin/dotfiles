6770247746173da0863c32d9e0512aa38d919b57
Alex Mazanov <alexandr.mazanov@gmail.com>
2022-09-04 15:26:16 -0400

    Add VSCode extensions install script

---
+++ .vscode-extensions.sh
@@ -0,0 +1,55 @@
+# Created with
+# code --list-extensions |
+#       xargs -L 1 echo code --install-extension |
+#       sed 's/$/ --force/' |
+#       sed '$!s/$/ \&/' > vscode-extensions.sh
+
+code --install-extension alefragnani.project-manager --force &
+code --install-extension amazonwebservices.aws-toolkit-vscode --force &
+code --install-extension bigous.vscode-multi-line-tricks --force &
+code --install-extension bradgashler.htmltagwrap --force &
+code --install-extension bradlc.vscode-tailwindcss --force &
+code --install-extension bungcip.better-toml --force &
+code --install-extension christian-kohler.npm-intellisense --force &
+code --install-extension dbaeumer.vscode-eslint --force &
+code --install-extension deerawan.vscode-dash --force &
+code --install-extension donjayamanne.python-environment-manager --force &
+code --install-extension DotJoshJohnson.xml --force &
+code --install-extension eamodio.gitlens --force &
+code --install-extension EditorConfig.EditorConfig --force &
+code --install-extension esbenp.prettier-vscode --force &
+code --install-extension fabiospampinato.vscode-open-in-finder --force &
+code --install-extension fabiospampinato.vscode-open-in-github --force &
+code --install-extension file-icons.file-icons --force &
+code --install-extension formulahendry.auto-rename-tag --force &
+code --install-extension gamunu.vscode-yarn --force &
+code --install-extension GitHub.copilot --force &
+code --install-extension GitHub.vscode-pull-request-github --force &
+code --install-extension golang.go --force &
+code --install-extension humao.rest-client --force &
+code --install-extension janisdd.vscode-edit-csv --force &
+code --install-extension KevinRose.vsc-python-indent --force &
+code --install-extension mechatroner.rainbow-csv --force &
+code --install-extension mohsen1.prettify-json --force &
+code --install-extension mquandalle.graphql --force &
+code --install-extension mrmlnc.vscode-duplicate --force &
+code --install-extension ms-azuretools.vscode-docker --force &
+code --install-extension ms-python.python --force &
+code --install-extension ms-python.vscode-pylance --force &
+code --install-extension ms-toolsai.jupyter --force &
+code --install-extension ms-toolsai.jupyter-keymap --force &
+code --install-extension ms-toolsai.jupyter-renderers --force &
+code --install-extension ms-vscode-remote.remote-containers --force &
+code --install-extension ms-vscode-remote.remote-ssh --force &
+code --install-extension ms-vscode-remote.remote-ssh-edit --force &
+code --install-extension ms-vscode.sublime-keybindings --force &
+code --install-extension njpwerner.autodocstring --force &
+code --install-extension rust-lang.rust-analyzer --force &
+code --install-extension samuelcolvin.jinjahtml --force &
+code --install-extension serayuzgur.crates --force &
+code --install-extension SimonSiefke.svg-preview --force &
+code --install-extension streetsidesoftware.code-spell-checker --force &
+code --install-extension technosophos.vscode-make --force &
+code --install-extension tommorris.mako --force &
+code --install-extension wholroyd.jinja --force &
+code --install-extension yzhang.markdown-all-in-one --force
